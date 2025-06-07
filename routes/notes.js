3a17ec5315597c88b9b222c4a8745dadb501773c
Noriko Iimura <2301355022ro@cyber-u.ac.jp>
2025-06-07 19:43:20 +0900

    reupload yesno

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://Niimue:HAxVhmBjJYwdutuI@cluster0.qe4su70.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "******";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
